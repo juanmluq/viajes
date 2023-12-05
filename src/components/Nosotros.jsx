c87f677e16a60c7c03f118e7f379abf2e7321694
juanmluq <soyluque@hotmail.com>
2023-12-05 14:19:31 -0300

    actualizacion condiciones

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Nosotros.css";
+import { BsWhatsapp, BsFacebook, BsInstagram, BsFillPhoneVibrateFill  } from "react-icons/bs";
+import { BiMailSend } from "react-icons/bi";
 
 export default function Nosotros(){
 
@@ -8,7 +10,7 @@ export default function Nosotros(){
     <div className="nos">
             <h3>CONDICIONES </h3>
             <h7>
-<br />​
+<br />
 
 * Alteraciones o modificaciones:
 <br />
@@ -18,7 +20,7 @@ La presente organización de viajes se reserva el derecho por razones técnicas 
 
 * Responsabilidad:
 <br />
-La presente organización de viajes declara expresamente que actúa con carácter de intermediaria en la reserva o contratación de los distintos servicios vinculados e incluidos en el respectivo tour o reservación de servicios, hoteles, restaurantes, medios de transporte (aéreos y terrestres según corresponda) u otros prestadores. La organizacion no se responsabiliza por los hechos que se produzcan por caso fortuito o fuerza mayor, fenómenos climáticos o hechos que acontezcan antes o durante el desarrollo del tour que impidan, demoren o de cualquier modo obstaculicen la ejecución total o parcial de las prestaciones comprometidas, de conformidad con lo dispuesto por el Código Civil. Por consiguiente declina todo tipo de responsabilidad sobre daño, pérdidas, robos o perjuicios que pudieran sufrir las personas, sus bienes, cualquiera fueran las causas ajenas que lo originen. Tampoco se considera responsable por las modificaciones de horarios y/o cambios de vuelos resueltos unilateralmente por las líneas aéreas participantes.
+La presente organización de viajes declara expresamente que actúa con carácter de intermediaria en la reserva o contratación de los distintos servicios vinculados e incluidos en el respectivo tour o reservación de servicios, hoteles, restaurantes, medios de transporte (aéreos y terrestres según corresponda) u otros prestadores. La organizacion no se responsabiliza por los hechos que se produzcan por caso fortuito o fuerza mayor, fenómenos climáticos o hechos que acontezcan antes o durante el desarrollo del tour que impidan, demoren o de cualquier modo obstaculicen la ejecución total o parcial de las prestaciones comprometidas, de conformidad con lo dispuesto por el Código Civil. Por consiguiente declina todo tipo de responsabilidad sobre daño, pérdidas, robos o perjuicios que pudieran sufrir las personas, sus bienes, cualquiera fueran las causas ajenas que lo originen. Tampoco se considera responsable por las modificaciones de horarios y/o cambios de vuelos resueltos unilateralmente por las líneas aéreas participantes.
 <br />
 <br />
 
@@ -28,7 +30,7 @@ La presente organizacion de viajes se reserva el derecho de hacer que abandone e
 <br />
 <br />
 
-* Documentación: 
+* Documentación:
 <br />
 La presente no se responsabiliza si por la falta de dicha documentación no se brindaren los servicios contratados, ni efectuará devoluciones de valores pagados por la misma causa. Es responsabilidad del viajero poseer en su poder todo lo necesario para su identificación personal, antes, durante y a la llegada del mismo o a su regreso al origen. Esto es sumamente importante cuando se trata de extranjeros, residentes, menores, etc. Mayor información será brindada por las líneas aéreas participantes y/o consulados respectivos.
 <br />
@@ -46,7 +48,7 @@ Estadías, comidas y/o gastos adicionales o perjuicios producidos por cancelacio
 <br />
 Alojamiento por la cantidad de noches especificadas y mencionadas en los itinerarios y/o vouchers, en hoteles especificados u otros de igual o mayor categoría, ocupando habitaciones según la elegida, con baño privado e impuestos. Régimen de comidas, según lo especificado en el voucher según se indique en cada oportunidad y lugar. Visitas y excursiones que se incluyan en el servicio adquirido y detalladas en el voucher, lo mismo que los traslados hasta y desde los aeropuertos, terminales, hoteles según se indique. La hotelería considera día completo el de llegada, no existiendo reclamos si las diferencias horarias acortan el período de alojamiento. Las habitaciones deben ser desocupadas a las 10:00 a.m. quedando a disposición del pasajero, las instalaciones del hotel y/o depósito de equipajes hasta la hora que el pasajero retire los mismos. Para la asignación de las habitaciones, las mismas se asignarán al arribo de los pasajeros en un todo de acuerdo a la adquirida y especificada en el voucher, según disponibilidad existente al momento del check-in. La diferencia que corresponda por cualquier cambio de categoría en la habitación asignada será abonada por el pasajero. Todas las habitaciones son de categoría standard, excepto que se especifique lo contrario en la documentación emitida. El pasajero podrá convenir con el operador local el cambio de los horarios de las excursiones incluidas, siempre que pueda satisfacerse su pedido, caso contrario, se ajustará a lo regular y programado. Para el caso de cualquier contratación de excursiones opcionales, las mismas son por cuenta y riesgo del pasajero al momento de contratarlas, no existiendo responsabilidad de la presente ni por la realización de la misma, ni por el monto abonado, ya que se entiende que es ajena a los servicios adquiridos y/o convenidos.
 <br />
-​<br />
+<br />
 
 * Solicitudes y pagos:
 <br />
@@ -66,7 +68,7 @@ Si la cancelación se produce 30 días antes de la salida, el cargo será del 50
 <br />
 <br />
 
-* Equipaje: 
+* Equipaje:
 <br />
 No será objeto de contrato de transporte entendiendo que el pasajero conserva siempre consigo el mismo por su cuenta y cargo, cualquiera sea el lugar donde es transportado. La presente no se verá obligado a responder contractualmente por la pérdida o daños que pudieren sufrir los mismos.
 <br />
@@ -80,33 +82,25 @@ El ticket en poder del pasajero será el contrato de transporte entre el pasajer
         <br />
         <br />
 
-        <div className='fondodos'>
-        <a style={{ width:"auto"}} href="https://api.whatsapp.com/send?phone=+5491127381642">  
+        <footer className='piePagin'>
 
-        <div className='piepag'>
-          Whatsapp
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-  <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
-</svg>
-        </div>
-        </a>
-        <div className='piepag'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-at" viewBox="0 0 16 16">
-  <path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
-  <path d="M14.247 14.269c1.01 0 1.587-.857 1.587-2.025v-.21C15.834 10.43 14.64 9 12.52 9h-.035C10.42 9 9 10.36 9 12.432v.214C9 14.82 10.438 16 12.358 16h.044c.594 0 1.018-.074 1.237-.175v-.73c-.245.11-.673.18-1.18.18h-.044c-1.334 0-2.571-.788-2.571-2.655v-.157c0-1.657 1.058-2.724 2.64-2.724h.04c1.535 0 2.484 1.05 2.484 2.326v.118c0 .975-.324 1.39-.639 1.39-.232 0-.41-.148-.41-.42v-2.19h-.906v.569h-.03c-.084-.298-.368-.63-.954-.63-.778 0-1.259.555-1.259 1.4v.528c0 .892.49 1.434 1.26 1.434.471 0 .896-.227 1.014-.643h.043c.118.42.617.648 1.12.648Zm-2.453-1.588v-.227c0-.546.227-.791.573-.791.297 0 .572.192.572.708v.367c0 .573-.253.744-.564.744-.354 0-.581-.215-.581-.8Z"/>
-</svg>
-      <div>eventosant@gmail.com
-      </div>
-        </div>
-          <a href="https://web.facebook.com/profile.php?id=100064332926542">
-        <div className='piepag'>
-        Facebook
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-</svg>
-      </div>
-          </a>
-      </div>
+<div style={{display: "flex", position: "relative", justifyContent: "space-between", margin:"0% 5%" }}>
+  <div> <BsFillPhoneVibrateFill/> (011) 2738 1642 </div>
+  <div> <BiMailSend/> eventosant@gmail.com </div>
+
+</div>        
+<div style={{display: "flex", position: "relative", justifyContent:"space-between", margin:"0% 5%" }}>
+<a style={{ width:"auto", color:"white"}} href="https://api.whatsapp.com/send?phone=+5491127381642"> <BsWhatsapp /> Whatsapp 
+</a>
+{/* <a style={{color: "white"}} href='https://www.instagram.com/potenciarcash/'><BsInstagram/> Instagram</a> */}
+<a style={{color: "white"}} href='https://web.facebook.com/profile.php?id=100064332926542'> <BsFacebook/> Facebook</a>
+</div>
+<div style={{display: "flex", position: "relative", justifyContent:"center" }}>
+  Eventos Ant. organiza tu destino!
+  <br />
+</div>
+<a style={{display: "flex", position: "relative", justifyContent:"center", paddingBottom:"10px" }} href="/condiciones">Terminos Y Condiciones</a> 
+</footer>
       </div>
     )
 }
