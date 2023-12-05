96fa03a80ce003809fdb59464d840e6b8b880040
juanmluq <soyluque@hotmail.com>
2023-12-05 02:13:02 -0300

    carga backend

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getPasajeros(){
     return async function(dispatch){
-        var json = await axios.get("http://localhost:3001/pasajeros");
+        var json = await axios.get("https://backpasajeros-production.up.railway.app/pasajeros");
         return dispatch({
             type: "GET_PASAJEROS",
             payload: json.data
@@ -13,7 +13,7 @@ export function getPasajeros(){
 
 export function postPasajeros(payload){
      return async function(dispatch) {
-        const response= await axios.post("http://localhost:3001/pasajero", payload); //axios.post dispara una ruta de post
+        const response= await axios.post("https://backpasajeros-production.up.railway.app/pasajero", payload); //axios.post dispara una ruta de post
         return response
      }
 }
