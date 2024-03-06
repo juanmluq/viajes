675b7e562863d8ff1de9e5c6cf32abf67330ce93
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-03-06 17:35:30 -0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import {Link, NavLink} from 'react-router-dom';
-
 import './Nav.css';
 
 function Nav() {
   return (
-    <nav className="nav nav-tabs" >
-                    
+    <nav className="nav nav-tabs" >           
       <li class="nav-item">
         <a class="nav-link " aria-current="page" href="/">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="/packs">Salidas</a>
       </li>
-   
       <li class="nav-item">
         <a class="nav-link" href="/contacto">Contacto</a>
       </li>
-
       <div className='navBtn'>  
         <Link to="/presupuestar" >
-        <button className='btn'>Presupuestar</button>
+          <button className='btn'>Presupuestar</button>
         </Link>
-        </div>
-        
+      </div>
     </nav>
   );
 };
